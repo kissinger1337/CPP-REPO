1ff0eb447d165857bb7da79d62602bb4bdc73786
Alexandr Dudkin <dudkiale@cvut.cz>
2023-06-17 22:05:10 +0200

    BFS initial commit

---
+++ BFS/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class CBFS{
+
+    enum State{Unvisited, Visited, Visiting};
+
+    struct TNode{
+
+    };
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
