dfbca8f71f22d7d3e8680579f2efb5a008a0761c
Alexandr Dudkin <dudkiale@cvut.cz>
2023-06-19 13:06:31 +0200

    Naive implementation of the BFS, very slow but works

--- BFS/main.cpp
+++ BFS/main.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 #include <filesystem>
 #include <map>
+#include <set>
 #include <vector>
 #include <cassert>
+#include <queue>
 
 /**
  * @brief class representing public transport lines. Contains functions to read from a file and find the shortest path using BFS.
@@ -12,11 +14,14 @@ class CPublicTransport{
 public:
     bool read (const std::string &);/**< Iterates through all files in one directory. Does not recursively iterate through deeper directories.*/
 
-    std::map<std::pair<std::string, std::string>, std::string>& getAllConnections(); /**< getter used only for testing */
+    std::map<std::pair<std::string, std::string>, std::string> getAllConnections() const; /**< Getter used only for testing */
+
+    bool find (const std::string & from, const std::string & to);
+
 private:
     bool readOneFile(const std::string &);
     //                   from          to           line name
-    std::map<std::pair<std::string, std::string>, std::string> m_AllConnections;
+    std::map<std::pair<std::string, std::string>, std::string> m_AllConnections;/**< map of all straight connections */
 };
 
 bool CPublicTransport::read(const std::string & path) {
@@ -40,20 +45,44 @@ bool CPublicTransport::readOneFile(const std::string & path) {
     getline(ifs, lineName);
 
     std::string stop;
-    std::vector<std::string> stops;
+    std::set<std::string> stops;
     while(getline(ifs, stop)){
-        for(auto other : stops) {
+        for(const auto& other : stops) {
+            //                                          from      to       line name
             m_AllConnections.emplace(std::make_pair(stop, other), lineName);
             m_AllConnections.emplace(std::make_pair(other, stop), lineName);
         }
-        stops.push_back(stop);
+        stops.insert(stop);
     }
 
     return true;
 }
 
-std::map<std::pair<std::string, std::string>, std::string> & CPublicTransport::getAllConnections() {
+std::map<std::pair<std::string, std::string>, std::string> CPublicTransport::getAllConnections() const {
     return m_AllConnections;
+}
+
+bool CPublicTransport::find(const std::string &from, const std::string &to) {
+    if(from == to) return true;
+
+    std::queue<std::string> toVisit;
+    std::set  <std::string> visited;
+
+    toVisit.push(from);
+    visited.insert(from);
+
+    while(!toVisit.empty()){
+        std::string f = toVisit.front();
+        toVisit.pop();
+        for(const auto & it : m_AllConnections){
+            if(it.first.first == f && visited.find(it.first.second) == visited.end() ){
+                toVisit.push(it.first.second);
+            }
+            visited.insert(f);
+        }
+    }
+    return visited.count(to) == 1;
+
 }
 
 int main() {
@@ -62,10 +91,17 @@ int main() {
     if(!a.read("/home/fitslave/code/CPP-REPO/BFS/lines"))
         std::cout << "Error opening file" << std::endl;
 
-    assert((a.getAllConnections().find({"Dejvická", "Depo Hostivař"}))->second == "A");
-    assert((a.getAllConnections().find({"Depo Hostivař", "Dejvická"}))->second == "A");
-    assert((a.getAllConnections().find({"Jinonice", "Českomoravská"}))->second == "B");
-    assert((a.getAllConnections().find({"Ládví", "Háje"}))->second == "C");
+    std::map<std::pair<std::string, std::string>, std::string> allConnectionsA = a.getAllConnections();
+
+    assert((allConnectionsA.find({"Dejvická", "Depo Hostivař"}))->second == "A");
+    assert((allConnectionsA.find({"Depo Hostivař", "Dejvická"}))->second == "A");
+    assert((allConnectionsA.find({"Jinonice", "Českomoravská"}))->second == "B");
+    assert((allConnectionsA.find({"Ládví", "Háje"}))->second == "C");
+    assert(allConnectionsA.find({"Dejvická","Háje"}) == allConnectionsA.end());
+
+    assert(a.find("Dejvická","Háje") == true);
+    assert(a.find("Dejvická","Anděl") == true);
+    assert(a.find("Dejvická","Ne existuje") == false);
 
     return 0;
 }
